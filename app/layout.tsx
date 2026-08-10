98bbd710cced827b3102d4dc020c97539cac9dd6
youngsikshin <14900599+youngsikshin@users.noreply.github.com>
2026-08-10 13:50:39 +0900

    Update browser tab icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ export const metadata: Metadata = {
   title: "RoMA Lab | Robotics, Mobility and AI Lab at KNU",
   description: "Robotics, Mobility and AI Lab at Kyungpook National University. 경북대학교 AI 로봇 모빌리티 연구실.",
   icons: {
-    icon: "/roma-logo-transparent-v3.png",
-    shortcut: "/roma-logo-transparent-v3.png",
+    icon: "/roma-favicon.png",
+    shortcut: "/roma-favicon.png",
   },
 };
 
